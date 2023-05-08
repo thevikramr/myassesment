11b35b9c4c5e133db52a34c0a57225dd224547d9
thevikramr <132732496+thevikramr@users.noreply.github.com>
2023-05-08 21:43:16 +0530

    Add files via upload
    day2

---
+++ Bike.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class Bike {
+	public static void main(String[] args) {
+		int noOfWheels=2;
+		System.out.println(noOfWheels);
+		short noOfMirrors=2;
+		System.out.println(noOfMirrors);
+		long chassisNumber=105764338;
+		System.out.println(chassisNumber);
+		boolean isPunctured=false;
+		System.out.println(isPunctured);
+		String bikeName="ktm";
+		System.out.println(bikeName);
+		double runningKM=59000;
+		System.out.println(runningKM);
+
+
+
+	}
+
+}
---
+++ Mobile.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class Mobile {
+	public static void main(String[] args) {
+
+		String mobileBrandName="Mi 11 Lite";
+		System.out.println(mobileBrandName);
+		char mobileLogo='M';
+		System.out.println(mobileLogo);
+		short noOfMobilePiece=1;
+		System.out.println(noOfMobilePiece);
+		int modelNumber=11;
+		System.out.println(modelNumber);
+		long mobileImeiNumber=890343298756101l;
+		System.out.println(mobileImeiNumber);
+		float mobilePrice=240000;
+		System.out.println(mobilePrice);
+		boolean isDamaged=false;
+		System.out.println(isDamaged);
+
+	}
+
+}
